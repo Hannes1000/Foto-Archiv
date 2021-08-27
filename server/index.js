446f6ffb30214a758fd0cd2186535a3bdc135808
Hannes1000 <60342304+Hannes1000@users.noreply.github.com>
2021-08-27 20:41:14 +0200

    test

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     //res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
     //res.sendFile(path.join(__dirname, "index.js"));
-    //res.send(<p>Seite nicht vorhanden</p>)
+    res.send("<p>Seite nicht vorhanden</p>")
   });
 }
 
